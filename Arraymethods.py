5be42d04e5effb1cdb11088f9ae872c4a854a747
Ravi Rathod <109081279+RaviRathod321@users.noreply.github.com>
2023-10-20 11:34:01 +0530

    Update Arraymethods.py

--- Arraymethods.py
+++ Arraymethods.py
@@ -9,8 +9,7 @@ import numpy as np
 # ndarray.argmin
 a = np.array([1, 2, 3, 4, 5, 23, 7, 8])
 # print(a.reshape(2, 4))
-#a = np.array([[1, 2],
-#             [7, 8]])
+#a = np.array([[1, 2]])
 #print(a.sum())
 #print(a.max(axis=1))
 #print(a.argmax()) #returns the value of the maximum number
